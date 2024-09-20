Fix Backprop
Back propagation formulas fixed
Need to implement softmax and then debug the training process

layers.py
@@ -1,49 +1,67 @@
 import numpy as np
 #Implement our own Neural Network
-def relu_derivative(x):
-        if x > 0:
-            return 1
-        else:
-            return 0
-def sigmoid_derivative(x):
-    return x * (1 - x)
-class Dense():
+class Layer:
+    def __init__(self):
+        self.input = None
+        self.output = None
+        self.name = None
+        pass
+    def forward(self, input):
+        #return output
+        pass
+    def backward(self, output_gradient, learning_rate):
+        #return input_error
+        pass
+
+
+class Dense(Layer):
 
     def __init__(self, input_size, output_size):
+        self.weights = np.random.randn(output_size, input_size)
+        self.bias = np.random.randn(output_size,1)
+        self.name = "Dense"
 
-        self.input_size = input_size
-        self.output_size = output_size
-        self.weights = [[round(np.random.randn()*0.01,10) for _ in range(input_size)] for _ in range(output_size)]
-        self.bias = [0 for _ in range(output_size)]
-        self.delta_weights = [[0 for _ in range(input_size)] for _ in range(output_size)]
-        self.delta_bias = [0 for _ in range(output_size)]
-        self.output = [0 for _ in range(output_size)]
-        self.input = [0 for _ in range(input_size)]
-
-    def compute_output(self, input,activation=lambda x : x):
+    def forward(self, input):
         self.input = input
-        output = [0 for _ in range(self.output_size)]
-        for i in range(self.output_size):
-            for j in range(self.input_size):
-                self.output[i] += input[j] * self.weights[i][j]
-            self.output[i] += self.bias[i]
-            self.output[i] = round(self.output[i],10)
-            output[i] = activation(self.output[i])
-        return output
+        self.output = self.weights.dot(input) + self.bias
+        return self.output
+
+    def backward(self, output_gradient, learning_rate):
+        m = output_gradient.shape[1]
+        input_gradient = self.weights.T.dot(output_gradient)
+        self.weights-= learning_rate * (output_gradient.dot(self.input.T) / m)
+        self.bias -= learning_rate * (np.sum(output_gradient,axis=1,keepdims=True)/ m)
+        return input_gradient
     
-    def backpropagate(self, cost):
-        new_cost = [0 for _ in range(self.output_size)]
-        for i in range(self.output_size):
-            z = self.output[i]
-            daz = sigmoid_derivative(z)
-            dca = cost[i]
-            for j in range(self.input_size):
-                self.delta_weights[i][j] += dca * self.input[j] * daz
-                new_cost[i] += dca * daz * self.weights[i][j]
-            self.delta_bias[i] += dca * daz
-        return new_cost
-    def update(self, learning_rate):
-        for i in range(self.output_size):
-            for j in range(self.input_size):
-                self.weights[i][j] -= learning_rate * self.delta_weights[i][j]
-            self.bias[i] -= learning_rate * self.delta_bias[i]
+class Activation(Layer):
+
+    def __init__(self, activation, activation_prime):
+        self.activation = activation
+        self.activation_prime = activation_prime
+        self.name = None
+
+    def forward(self, input):
+        self.input = input
+        return np.vectorize(self.activation)(self.input)
+
+    def backward(self, output_gradient, learning_rate):
+        res = np.vectorize(self.activation_prime)(self.input)
+        return np.multiply(output_gradient, np.vectorize(self.activation_prime)(self.input))
+    
+class Relu(Activation):
+    def __init__(self):
+        relu = lambda x : np.maximum(0, x)
+        relu_prime = lambda x : x>0
+        super().__init__(relu, relu_prime)
+        self.name = "Relu"
+
+class Softmax(Layer):
+    def __init__(self):
+        self.name = "Softmax"
+    def forward(self, input):
+        e_input = np.exp(input)
+        self.output = e_input / np.sum(e_input)
+        return self.output
+    def backward(self, output_gradient, learning_rate):
+        I = np.eye(self.output.shape[0])
+        return output_gradient.dot((I - self.output.T).T)

main.py
@@ -1,42 +1,32 @@
 import network
 import layers
+import numpy as np
 from keras.datasets import mnist
 from keras.utils import to_categorical
 
-train_data = mnist.load_data()[0][0]
-flatten_train_data = []
-for data in train_data:
-    flatten_train_data.append(data.flatten())
-train_labels = to_categorical(mnist.load_data()[0][1])
 
-test_data = mnist.load_data()[1][0]
-flatten_test_data = []
-for data in test_data:
-    flatten_test_data.append(data.flatten())
-test_labels = to_categorical(mnist.load_data()[1][1])
 
-network = network.Network(learning_rate=0.05,batch_size=256)
+def preprocess(x,y):
+    x = x.reshape(x.shape[0], 28*28)
+    x = x.astype("float32") / 255
+    y = to_categorical(y)
+    y = y.reshape(y.shape[1],y.shape[0])
+    return x.T,y
 
-network.add_layer(layers.Dense(784, 10))
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-network.train(flatten_train_data, train_labels)
+x_train, y_train = preprocess(x_train, y_train)
+x_test, y_test = preprocess(x_test, y_test)
+model = network.Network()
+model.add_layer(layers.Dense(784, 40))
+model.add_layer(layers.Relu())
+model.add_layer(layers.Dense(40, 10))
+model.add_layer(layers.Relu())
 
-network.save()
-"""
-model = network.Network.load_model()
+model.train(x_train, y_train, batch_size=64, epochs=100, learning_rate=0.06, verbose=True)
 
-pred = model.predict(flatten_test_data)
-for i,p in enumerate(pred):
-    pred[i] = p.index(max(p))
-for i,l in enumerate(test_labels):
-    max_val = 0
-    max_index = 0
-    for j in range(len(l)):
-        if l[j] > max_val:
-            max_val = l[j]
-            max_index = j
-    test_labels[i] = max_index
-for p, label in zip(pred, test_labels):
-    print(p, label)
+#network.save()
 
-"""
+for x,y in zip(x_test, y_test):
+    output = model.predict(x_test)
+    print('pred:', np.argmax(output), '\ttrue:', np.argmax(y))

network.py
@@ -1,65 +1,58 @@
 import layers
 import math
+import numpy as np
+
+def mse(y_true, y_pred):
+    return np.mean(np.power(y_true-y_pred,2))
+def mse_prime(y_true, y_pred):
+    return 2 * (y_pred - y_true) / np.size(y_true)
 
 class Network:
-    def __init__(self, learning_rate=0.7, batch_size=1) -> None:
+    def __init__(self):
         self.layers = []
-        self.learning_rate = learning_rate
-        self.batch_size = batch_size
 
     def add_layer(self, layer):
         self.layers.append(layer)
 
-    def train(self, data, labels):
-        for i in range(0, len(data), self.batch_size):
-            limit = min(i + self.batch_size, len(data))
-            predictions = self.predict(data[i:limit])
-            mse = 0
-            gradients = []
-            for prediction, label in zip(predictions, labels[i:limit]):
-                for i in range(10):
-                    mse += (prediction[i] - label[i])**2
-                    gradients.append((prediction[i] - label[i]) * 2)
-            mse /= len(data)
-            print(mse)
-            for layer in reversed(self.layers):
-                gradients = layer.backpropagate(gradients)
-            for layer in self.layers:
-                layer.update(learning_rate=self.learning_rate)
+    def train(self, data, labels, batch_size = 1, epochs = 100, learning_rate = 0.05, verbose = True):
+        for e in range(epochs):    
+            mean_squared_error = 0
+            for i in range(0, len(data), batch_size):
+                limit = min(i + batch_size, len(data))
+                cur_data = data[:,i:limit]
+                cur_labels = labels[:,i:limit]
+                predictions = self.predict(cur_data)
+                mean_squared_error += mse(cur_labels,predictions)
+                gradients = mse_prime(cur_labels,predictions)
+                for layer in reversed(self.layers):
+                    gradients = layer.backward(gradients, learning_rate)
+            if verbose:
+                print(f"{e + 1}/{epochs}, error={mean_squared_error}")
     def predict(self, data):
-        predictions = []
-        for element in data:
-            for layer in self.layers:
-                element = layer.compute_output(input=element,activation=self.sigmoid)
-            predictions.append(element)
+        predictions = data
+        for layer in self.layers:
+            predictions = layer.forward(predictions)
         return predictions
-    def relu(self, x):
-        return max(0, x)
-    def sigmoid(self, x):
-        return 1 / (1 + math.exp(-x))
-    def save(self):
-        with open('models/custom_model.h5', 'w') as f:
-            f.write(str(self.learning_rate) + "\n")
-            f.write(str(self.batch_size) + "\n")
+    
+    def save(self, path="models/custom_model.h5"):
+        #TODO
+        with open(path, 'w') as f:
             f.write(str(len(self.layers)) + "\n")
             for layer in self.layers:
-                f.write(str(layer.input_size) + "\n")
-                f.write(str(layer.output_size) + "\n")
-                for line in layer.weights:
-                    f.write(" ".join([str(x) for x in line]) + "\n")
-                for line in layer.bias:
-                    f.write(str(line) + "\n")
+                layer.save(path)
     def load_model(path='models/custom_model.h5'):
+        #TODO
         model = Network()
         with open(path, 'r') as f:
-            model.learning_rate = float(f.readline())
-            model.batch_size = int(f.readline())
             model.layers = []
             num_layers = int(f.readline())
             for i in range(num_layers):
-                model.layers.append(layers.Dense(int(f.readline()), int(f.readline())))
-                for line in f:
-                    model.layers[-1].weights.append([float(x) for x in line.split()])
-                for line in f:
-                    model.layers[-1].bias.append(float(line))
+                new_layer = layers.Layer()
+                new_layer.name = f.readline()
+                if new_layer.name == "Dense":
+                    model.layers.append(layers.Dense())
+                    for line in f:
+                        model.layers[-1].weights.append([float(x) for x in line.split()])
+                    for line in f:
+                        model.layers[-1].bias.append(float(line))
         return model